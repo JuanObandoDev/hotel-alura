66fe63edbc3da02b86228bf10d89a7cc69f9150c
Juan Obando <juanobando.developer@gmail.com>
2023-09-11 14:49:11 -0500

    feat: create logic to search a reservation by different parameters

--- src/dao/ReservationDAO.java
+++ src/dao/ReservationDAO.java
@@ -98,6 +98,33 @@ public class ReservationDAO {
         return reservations;
     }
 
+    public List<Reservation> getReservationsBySearch(String search) throws SQLException {
+        List<Reservation> reservations = new ArrayList<>();
+        final PreparedStatement ps = this.conn.prepareStatement(
+                "SELECT * FROM reservations WHERE id LIKE ? OR start_date LIKE ? OR end_date LIKE ? OR total LIKE ? OR payment_method LIKE ?");
+        try (ps) {
+            ps.setString(1, "%" + search + "%");
+            ps.setString(2, "%" + search + "%");
+            ps.setString(3, "%" + search + "%");
+            ps.setString(4, "%" + search + "%");
+            ps.setString(5, "%" + search + "%");
+            final ResultSet rs = ps.executeQuery();
+            try (rs) {
+                while (rs.next()) {
+                    reservations.add(new Reservation(
+                            rs.getInt("id"),
+                            rs.getString("start_date"),
+                            rs.getString("end_date"),
+                            rs.getInt("total"),
+                            rs.getString("payment_method")));
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return reservations;
+    }
+
     public int getLastReservationId() throws SQLException {
         final PreparedStatement ps = this.conn.prepareStatement(
                 "SELECT id FROM reservations ORDER BY id DESC LIMIT 1");
